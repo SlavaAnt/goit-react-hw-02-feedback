cd7b0ca709411d21d5f92fee73171f9ed47b9ecf
Slava Antolyk <Sl.antolyk@gmail.com>
2023-02-25 21:16:29 +0200

    add feedback widget

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import FeedbackWidget from 'components/FeedbackWidget/FeedbackWidget';
+import 'index.css';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +11,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <FeedbackWidget initialValue={0} />
     </div>
   );
 };
